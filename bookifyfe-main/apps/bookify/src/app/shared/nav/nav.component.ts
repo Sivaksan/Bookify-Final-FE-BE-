1fbedf914d560885c4f8ad178be328adb065c8c6
S Sivakshan <sivapower2@gmail.com>
2023-02-01 19:11:05 +0530

    finalazin

---
+++ bookifyfe-main/apps/bookify/src/app/shared/nav/nav.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bookify-nav',
+  templateUrl: './nav.component.html',
+  styles: [
+  ]
+})
+export class NavComponent {
+
+}
